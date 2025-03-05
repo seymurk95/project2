23997ee7b58acd79afd2d2ae24744fdd3637361e
Seymur <42584517+seymurk95@users.noreply.github.com>
2025-03-05 10:35:52 +0400

    Add files via upload

---
+++ projects/tokenizer.h
@@ -0,0 +1,41 @@
+//
+// Created by User on 01.03.2025.
+//
+
+#ifndef TOKENIZER_H
+#define TOKENIZER_H
+#include <string>
+#include <fstream>
+#include <iostream>
+enum class TOKEN{
+     CURLY_OPEN,
+     CURLY_CLOSE,
+     COLON,
+     STRING,
+     NUMBER,
+     ARRAY_OPEN,
+     ARRAY_CLOSE,
+     COMMA,
+     BOOLEAN,
+     NULL_TYPE
+};
+
+struct Token{
+  std::string value;
+  TOKEN type;
+  std::string toString;
+};
+
+class Tokenizer{
+  private:
+    std::fstream file;
+    size_t prevPos;
+    public:
+      Tokenizer(std::string fileName);
+      auto getWithoutWhiteSpace();
+      auto getToken();
+      auto hasMoreTokens();
+      void rollBackTokens();
+};
+
+#endif //TOKENIZER_H
