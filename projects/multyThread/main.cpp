ab32eef10f0063bcdc9c526f1c0aa207de2f8814
seymurk95 <seymur.kazimov.05@mail.ru>
2025-03-14 10:18:56 +0400

    changes

--- projects/multyThread/main.cpp
+++ projects/multyThread/main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <thread>
+#include<chrono>
+#include<condition_variable>
+#include<mutex>
+#include<string>
 /*int main() {
     unsigned int num_threads = std::thread::hardware_concurrency();
     if (num_threads != 0) {
         std::cout <<num_threads << std::endl;
     }
 }*/
-void foo(int z) {
+/*void foo(int z) {
     for (int i = 0; i != z; i++) {
         std::cout << "Thread using function foo \n";
 
@@ -48,5 +52,49 @@ int main() {
     th3.join();
     th4.join();
     th5.join();
+
+}*/
+
+std::mutex m;
+std::condition_variable cv;
+std::string data;
+bool ready = false;
+bool processed = false;
+void worker_thread() {
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout<<"Worker thread started"<<std::endl;
+    std::unique_lock lk(m);
+    cv.wait(lk,[]{return ready;});
+    std::cout<<"Worker thread inprocess"<<std::endl;
+    data+=" after processing";
+    processed = true;
+    std::cout<<"Worker thread signals processing complete"<<std::endl;
+    lk.unlock();
+    cv.notify_one();
+}
+
+void master_thread() {
+    std::cout<<"Master thread start"<<std::endl;
+    data = "Example data";
+    {
+        std::lock_guard lk(m);
+        ready = true;
+        std::cout<<"Master thread signals data ready for processing"<<std::endl;
+
+    }
+    cv.notify_one();
+    {
+        std::unique_lock lk(m);
+        cv.wait(lk,[]{return processed;});
+    }
+    std::cout<<"In master thread again. Data= "<<data<<std::endl;
+
+}
+
+int main() {
+    std::thread worker { worker_thread };
+    std::thread master { master_thread };
+    worker.join();
+    master.join();
 
 }
